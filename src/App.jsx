907a6dbd24aec5f745df98657c8b2efc3bdcabb4
ssuvamm <ssuvamm@gmail.com>
2024-09-27 12:26:28 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Bio from "./pages/Bio";
